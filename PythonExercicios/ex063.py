223db5a28499af862c47399613b50a2088910536
Symphoriano Martins Branco <104242815+SymMartins@users.noreply.github.com>
2022-04-27 15:22:43 -0300

    Exercicio 63 feito e entendido "Graças a Deus!!!!"

--- PythonExercicios/ex063.py
+++ PythonExercicios/ex063.py
@@ -1,3 +1,23 @@
 '''escreva um programa que leia um número N inteiro qualquer e mostre na tela os N primeiros
 elementos de uma sequencia Fibonacci:
 Ex: 0 - 1 - 1 - 2 - 3 - 5 - 8'''
+from time import sleep
+print('-=-' * 8)
+print('Sequencia de Fibonacci')
+print('-=-' * 8)
+sleep(1)
+print('~~' * 18)
+n = int(input('Quantos termos você quer mostrar? '))
+print('~~' * 18)
+t1 = 0
+t2 = 1
+print(f'{t1} - {t2}', end='')
+cont = 3
+while cont <= n:
+    t3 = t1 + t2
+    print(f' - {t3}', end='')
+    cont += 1
+    t1 = t2
+    t2 = t3
+print(' - FIM')
+print('~~' * 18)
