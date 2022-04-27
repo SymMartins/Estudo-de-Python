0e0438780fb6b40670e2bb7867399f5b3e72b283
Symphoriano Martins Branco <104242815+SymMartins@users.noreply.github.com>
2022-04-27 15:42:46 -0300

    Exercicio 64 feito e entendido "Graças a Deus!!!!"

--- PythonExercicios/ex064.py
+++ PythonExercicios/ex064.py
@@ -1,3 +1,11 @@
-'''Crie um número que leia VARIOS NÚMEROS inteiros pelo teclado. O programa só vai parar quando o usuario digitar o valor
-999 uqe é a CONDIÇÃO DE PARADA. No final, mostre quantos números foram digitados e qual foi a soma entre eles
-(desconsiderando o flag)'''
+''' Crie um programa que leia VARIOS NÚMEROS inteiros pelo teclado. O programa só vai parar quando o usuario digitar o valor
+999 que é a CONDIÇÃO DE PARADA. No final, mostre quantos números foram digitados e qual foi a soma entre eles
+(desconsiderando o flag) '''
+
+numero = cont = soma = 0
+numero = int(input('Digite um número [999 para parar]: '))
+while numero != 999:
+    cont += 1
+    soma += numero
+    numero = int(input('Digite um número [999 para parar]: '))
+print(f'Você digitou {cont} e a soma entre eles foi {soma}')
