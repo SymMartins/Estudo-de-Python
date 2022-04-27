e0f698b4c72584add003fb3f13778d07a88dad7e
Symphoriano Martins Branco <104242815+SymMartins@users.noreply.github.com>
2022-04-27 16:29:24 -0300

    Exercicio 65 feito com pequena dificuldade nos if do maior e menor!! (não lembrei como era feito) mas entendido como o código funciona! "Graças a Deus!!!!"

--- PythonExercicios/ex065.py
+++ PythonExercicios/ex065.py
@@ -1,2 +1,21 @@
-'''crie um programa que leia vários números inteiros pelo teclado. no final da execução, mostre a média entre todos os valores
-e qual foi o maior e o menor valores lidos. O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores'''
+""" Crie um programa que leia vários números inteiros pelo teclado.
+No final da execução, mostre a média entre todos os valores e qual foi o maior e o menor valores lidos.
+O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores."""
+
+resp = 'S'
+soma = cont = media = maior = menor = 0
+while resp in 'Ss':
+    numero = int(input('Digite um número: '))
+    soma += numero
+    cont += 1
+    if cont == 1:
+        maior = menor = numero
+    else:
+        if numero > maior:
+            maior = numero
+        if numero < menor:
+            menor = numero
+    resp = str(input('Deseja continuar? [S/N] ')).upper().strip() [0]
+media = soma / cont
+print(f'Você digitou {cont} número e a média foi {media}')
+print(f'O maior valor foi {maior} e o menor foi {menor}')
