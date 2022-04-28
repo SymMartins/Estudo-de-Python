78214e94543767f53b6c63ad2bf202c0049931b9
Symphoriano Martins Branco <104242815+SymMartins@users.noreply.github.com>
2022-04-28 09:37:15 -0300

    Na aula 15 tenho o exemplo de um while com loop infinito e parada pelo break e também como funciona a formatação com o "f" antes das aspas simples '' no print. Obs: A formatação de número deve ser transformadas em (int) ou (float). Agora partir para os DESAFIOS para treinar while com loop infinito!

--- PythonExercicios/mundo_dois/aula15.py
+++ PythonExercicios/mundo_dois/aula15.py
@@ -1,3 +1,21 @@
 """Nessa aula, vamos aprender como utilizar a instrução break e os loopings infinitos a favor das nossas estratégias de código.
 É muito importante saber usar o break no Python, já que em alguns casos precisamos interromper um laço no meio do caminho.
 Além disso, vamos aprender como trabalhar com as novas fstrings do Python."""
+#Ex de um while com loop infinito e parado pelo break
+n = s = 0
+while True:
+    n = int(input('Digite um número: '))
+    if n == 999:
+        break
+    s += n
+print(f'A soma vale {s}')
+
+"""nome = 'pedro'
+idade = 25
+peso = 59.6
+
+print(f'O {nome:-^20} tem {idade} e pesa {peso:.2f}')
+EX: de formatação com f.
+obs: números deve ser transformados em (int) ou (float)
+"""
+
