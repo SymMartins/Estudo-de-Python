c5fc1eedc92cc32fe544f1606aeb32fd518061b5
Symphoriano Martins Branco <104242815+SymMartins@users.noreply.github.com>
2022-04-28 09:51:05 -0300

    Exercicio de número 66 feito com sucesso!!!! Os DESAFIOS propóstos pelo prof. Gustavo Guanabar são realmente bões e ajudam a memorizar todo o conteúdo passado nas aulas!!

---
+++ PythonExercicios/mundo_dois/ex066.py
@@ -0,0 +1,12 @@
+""" Crie um programa que leia vários números inteiros pelo teclado.
+O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada.
+No final, mostre quantos números foram digitados e qual foi a soma entre eles (DESCONSIDERANDO O FLAG)"""
+
+n = c = s = 0
+while True:
+    n = int(input('Digite um número [999 para parar]: '))
+    if n == 999:
+        break
+    c += 1
+    s += n
+print(f'foram digitados {c} e a soma entre eles foi {s}')
