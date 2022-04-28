0c5741f53e3f2962b717ac7ea13bdd86c21fd14a
Symphoriano Martins Branco <104242815+SymMartins@users.noreply.github.com>
2022-04-28 00:25:31 -0300

    Organizando as aulas em mundos conforme está no Curso em Vídeo do prof. Gustavo Guanabara

---
+++ PythonExercicios/mundo_um/aula01-02-03-05.py
@@ -0,0 +1,13 @@
+"""AULA - 01: Você está pensando em ser um Programador e não sabe por onde começar?
+Pois nessa aula vamos te dar todas as informações e mostrar o caminho a seguir."""
+
+"""AULA - 02: De onde vem o Python? Por que esse nome?
+Quais são as grandes empresas mundiais que usam Python em seus softwares?
+Essas e muitas outras perguntas serão respondidas nessa aula."""
+
+"""AULA - 03: Entenda como funciona um Interpretador e veja como o Python funciona.
+Depois aprenda como instalar o Python 3.0 e o IDLE em seu computador com Windows, Linux ou Mac OS."""
+
+"""AULA - 05: Nesta aula, veremos como instalar e configurar a IDE (Integrated Development Environment)
+Python chamada PyCharm no Windows, MacOS e Linux.
+Além disso, veremos como programar Python no Android, utilizando o QPython3."""
---
+++ PythonExercicios/mundo_um/aula04.py
@@ -0,0 +1,7 @@
+""" Agora chegou a hora de aprender os comandos básicos do Python e fazer os primeiros programas em Linguagem Python."""
+# Toda variável é um objeto!
+
+nome = input('Digite seu nome: ')
+idade = input('Digite sua idade: ')
+peso = input('Digite seu peso: ')
+print('nome: ' + nome, 'idade: ' + idade, 'peso: ' + peso)
