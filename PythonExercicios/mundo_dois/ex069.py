75425c2a7a3ac97fccb26907c9a7004ca97bd82d
Symphoriano Martins Branco <104242815+SymMartins@users.noreply.github.com>
2022-04-28 16:29:30 -0300

    Os três últimos desafios vou deixar para fazer a noite! Depois deles vou fazer a prova e partir para o terceiro mundo!!!

---
+++ PythonExercicios/mundo_dois/ex069.py
@@ -0,0 +1,7 @@
+"""Crie um programa que leia a idade e o sexo de várias pessoas.
+A cada pessoa cadastrada, o preograma deverá perguntar se o usuário quer ou não continuar.
+no final mostre:
+
+A = quantas pessoas tem mais de 18 anos.
+b = Quantos homens foram cadastrados.
+c = quantas mulheres tem menos de 20 anos."""
---
+++ PythonExercicios/mundo_dois/ex070.py
@@ -0,0 +1,5 @@
+"""Crie um programa que leia o nome e o preço de vários produtos. O programa deverá perguntar se o usuário vai continuar.
+No final, mostre:
+A = Qual o total gasto na compra
+B = quantos produtos custam mais de R$: 1000,00.
+C = Qual o nome do produto mais barato. """
