9e14220db2263e82a01bf5eadfeb7e1277be9898
Symphoriano Martins Branco <104242815+SymMartins@users.noreply.github.com>
2022-04-28 16:23:53 -0300

    Este desafio foi tranquilo! Sem dificuldades resolvi na hora que o prof. Guanabara explicou o desafio!

--- PythonExercicios/mundo_dois/ex066.py
+++ PythonExercicios/mundo_dois/ex066.py
@@ -2,7 +2,7 @@
 O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada.
 No final, mostre quantos números foram digitados e qual foi a soma entre eles (DESCONSIDERANDO O FLAG)"""
 
-n = c = s = 0
+c = s = 0
 while True:
     n = int(input('Digite um número [999 para parar]: '))
     if n == 999:
