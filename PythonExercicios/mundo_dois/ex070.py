6e662a7a639ef227af04f74c8884190a645ac39b
Symphoriano Martins Branco <104242815+SymMartins@users.noreply.github.com>
2022-05-02 14:57:56 -0300

    Exercício 070 foi concluido. Um pouco de dificuldades em conseguir colocar o menor preço mas como tudo na vida é praticando que se memoriza e aprende!!! Hehehehe Partindo para o últomo exercício do mundo !!

--- PythonExercicios/mundo_dois/ex070.py
+++ PythonExercicios/mundo_dois/ex070.py
@@ -3,3 +3,33 @@ No final, mostre:
 A = Qual o total gasto na compra
 B = quantos produtos custam mais de R$: 1000,00.
 C = Qual o nome do produto mais barato. """
+print('-' * 30)
+print('{:-^30}'.format(' Lojinha do Sym '))
+print('-' * 30)
+
+total_compras = total_mil = menor_preco = cont = 0
+barato = ''
+while True:
+
+    cont += 1
+    produto = str(input('Digite um produto: ')).strip().title()
+    preco = float(input('Valor do produto R$: '))
+
+    total_compras += preco
+    if preco > 1000:
+        total_mil += 1
+    if cont == 1 or preco < menor_preco:
+        menor_preco = preco
+        barato = produto
+
+    sair = ' '
+    while sair not in 'SN':
+        sair = str(input('Deseja continuar cadastrando novos produtos [S/N]? ')).upper().strip()[0]
+    if sair == 'N':
+        break
+
+print(f'O total da sua compra foi de R$: {total_compras:.2f}.')
+print(f'Temos {total_mil} produtos custando mais de R$: 1000.00')
+print(f'O produto mais barato foi {barato} e ele custa R$: {menor_preco}')
+
+print('Cadastro de produtos finalizado.')
