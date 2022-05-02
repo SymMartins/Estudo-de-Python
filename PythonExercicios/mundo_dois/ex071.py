38f3e7ac88fbd37d2c639111aa1797e4ad059f76
Symphoriano Martins Branco <104242815+SymMartins@users.noreply.github.com>
2022-05-02 15:26:30 -0300

    Exercício 071 foi concluido. Confeço que quebrei a cabeça e só resolvi com a ajuda da resolução do exercício!! Mas entendi o fundamento da somatória das notas e como fazer o programa ir diminuindo e mostrando quantas notas eram necessárias!!!
    Partiu para Prova!!!

--- PythonExercicios/mundo_dois/ex071.py
+++ PythonExercicios/mundo_dois/ex071.py
@@ -3,3 +3,32 @@ No inicio, pergunte ao usuário qual será o valor a ser sacado (número inteiro
 cédulas de cada valor serão entregues.
 
 OBS: Considere que o caixa possui cédulas de R$: 50,00 - R$ 20,00 - R$ 10,00 e R$ 1,00."""
+
+print('=' * 35)
+print('{:-^35}'.format(' Banco do Sym Salamim '))
+print('=' * 35)
+valor = int(input('Qual o valor a ser sacado? '))
+total = valor
+ced = 50
+totalced = 0
+
+while True:
+
+    if total >= ced:
+        total -= ced
+        totalced += 1
+    else:
+        if totalced > 0:
+            print(f'Total de {totalced} cedulas de R$:{ced}')
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        totalced = 0
+        if total == 0:
+            break
+print('=' * 35)
+print('Volte sempre ao banco Sym Salamim')
+print('=' * 35)
