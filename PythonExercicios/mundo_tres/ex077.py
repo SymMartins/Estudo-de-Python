d4a67d2938c18b8a8b8c343f2886fabee77cf15a
Symphoriano Martins Branco <104242815+SymMartins@users.noreply.github.com>
2022-05-10 14:40:36 -0300

    Exercicío 077 completo!!
    Este exercicío foi tranquilo:
    
    obs: Tenho um for dentro de outro for o primeiro analiza a tupla e seu conteudo como um todo, já no segundo for analizo cada palavra individualmente para separar suas vogais!

--- PythonExercicios/mundo_tres/ex077.py
+++ PythonExercicios/mundo_tres/ex077.py
@@ -1,2 +1,13 @@
 """Crie um programa que tenha uma tupla com várias palavras (não usar acentos).
-Depois disso, você deve mostrar, para cada palavra, quais são as suas vogaais."""
+Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais."""
+
+# 1º Tupla com várias palavras sem usar acentos.
+palavras = ('casa', 'carro', 'apartamento', 'estacionamento', 'amizade',
+            'pessoas', 'amor', 'motocicleta', 'viagem', 'python',
+            'estudar', 'foco', 'ajudar', 'gratis')
+# 2º Mostra quais são as vogais de cada palavra.
+for palavra in palavras:
+    print(f'\nNa palavra {palavra.upper()}, temos as vogais: ', end='')
+    for letra in palavra:
+        if letra.lower() in 'aeiou':
+            print(letra, end=' ')
