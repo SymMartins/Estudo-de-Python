1d5e889e3bec6c928ba713e2158d5e87700bb9a6
Symphoriano Martins Branco <104242815+SymMartins@users.noreply.github.com>
2022-05-10 14:52:04 -0300

    Listas parte 1 Nessa aula, vamos aprender o que são LISTAS e como utilizar listas em Python. As listas são variáveis compostas que permitem armazenar vários valores em uma mesma estrutura, acessíveis por chaves individuais.

--- PythonExercicios/mundo_tres/aula17.py
+++ PythonExercicios/mundo_tres/aula17.py
@@ -1,3 +1,4 @@
-"""Nessa aula, vamos aprender o que são LISTAS e como utilizar listas em Python.
+"""Lista parte 1. Nessa aula, vamos aprender o que são LISTAS e como utilizar listas em Python.
 As listas são variáveis compostas que permitem armazenar vários valores em uma mesma estrutura,
 acessíveis por chaves individuais."""
+
