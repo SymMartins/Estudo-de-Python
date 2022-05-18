9f9a65be6ea8195165f2fc5c53f34f9a8610e67f
Symphoriano Martins Branco <104242815+SymMartins@users.noreply.github.com>
2022-05-18 14:35:56 -0300

    Exercício 85:
    Exercício completo!
    Exercício resolvido antes de ver a resolução do prof. Guanabara.
    Criei 3 listas uma para os números digitados outra para os números pares e outra para os números impares.
    Também fiz os dois modos de criar uma lista justamente para lembrar das duas maneira e ver qual fica melhor esteticamente dentro do código.
    
    modo um "nome = list()"
    modo dois "nome = []"
    
    Obs. Apesar da conveção dizer que conchetes "[]" quer dizer lista em PYTHON acho que estéticamente falando escrever ".list()" facilita a leitura do código para programadores de qualquer nivel.
    
    Obs. Eu irei utulizar sempre ".list()" por achar que facilita a leitura do código.
    
    Correção:
    Não prestei atenção então acabei criando o programa com 3 listas separadas uma para os números, uma para os pares e outra para os impares.
    Porém no enunciado estava pedindo para que fosse feito em uma lista única.

--- PythonExercicios/mundo_tres/ex85.py
+++ PythonExercicios/mundo_tres/ex85.py
@@ -1,5 +1,5 @@
 """Crie um programa onde o usuário possa digitar sete valores numéricos e
-cadastre-os em uma lista única que mantenha separados os valores pares e ímpares.
+cadastre-os em uma (lista única) que mantenha separados os valores pares e ímpares.
 No final, mostre os valores pares e ímpares em ordem crescente."""
 
 # 1.º Criar uma lista:
@@ -9,7 +9,7 @@ impar = []
 
 # 2.º Números adiconados pelo usuário, total de 7 números:
 for cont in range(0, 7):
-    numeros.append(int(input(f'Digite o {cont + 1}º número: ')))
+    numeros.append(int(input(f'Digite o {cont + 1}.º número: ')))
 
 # 3.º Separando os números pares e impares:
 for num in numeros:
@@ -20,6 +20,21 @@ for num in numeros:
         impar.append(num)
         impar.sort()
 # 4.º Mostra os valores pares e impares em ordem crescente:
-print('-='*30)
+print('-=' * 30)
 print(f'Os números pares são: {par}')
 print(f'Os números impares são: {impar}')
+print()
+
+# ------------------------------------ RESOLUÇÃO DO EX. 85 DA FORMA CORRETA --------------------------------------------
+
+numeros = [[], []]
+valor = 0
+for num in range(1,8):
+    valor = int(input(f'Digite o {num}.º valor: '))
+    if valor % 2  == 0:
+        numeros[0].append(valor)
+    else:
+        numeros[1].append(valor)
+print('-='*30)
+print(f'Os números pares são: {numeros[0]}')
+print(f'Os números impares são: {numeros[1]}')
