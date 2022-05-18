68f0aadfa1e96de808de257fdff8360f41e7aac9
Symphoriano Martins Branco <104242815+SymMartins@users.noreply.github.com>
2022-05-18 14:19:00 -0300

    Exercício 85:
    Exercício completo!
    Exercício resolvido antes de ver a resolução do prof. Guanabara.
    Criei 3 listas uma para os números digitados outra para os números pares e outra para os números impares.
    Também fiz os dois modos de criar uma lista justamente para lembrar das duas maneira e ver qual fica melhor esteticamente dentro do código.
    
    modo um "nome = list()"
    modo dois "nome = []"
    
    Obs. Apesar da conveção dizer que conchetes "[]" quer dizer lista em PYTHON acho que estéticamente falando escrever ".list()" facilita a leitura do código para programadores de qualquer nivel.
    
    Obs. Eu irei utulizar sempre ".list()" por achar que facilita a leitura do código.

---
+++ PythonExercicios/mundo_tres/ex85.py
@@ -0,0 +1,25 @@
+"""Crie um programa onde o usuário possa digitar sete valores numéricos e
+cadastre-os em uma lista única que mantenha separados os valores pares e ímpares.
+No final, mostre os valores pares e ímpares em ordem crescente."""
+
+# 1.º Criar uma lista:
+numeros = list()  # 1.º modo de criar uma lista
+par = []  # 2.º modo de criar uma lista
+impar = []
+
+# 2.º Números adiconados pelo usuário, total de 7 números:
+for cont in range(0, 7):
+    numeros.append(int(input(f'Digite o {cont + 1}º número: ')))
+
+# 3.º Separando os números pares e impares:
+for num in numeros:
+    if num % 2 == 0:
+        par.append(num)
+        par.sort()
+    else:
+        impar.append(num)
+        impar.sort()
+# 4.º Mostra os valores pares e impares em ordem crescente:
+print('-='*30)
+print(f'Os números pares são: {par}')
+print(f'Os números impares são: {impar}')
