9484fff28c560f59ea4c7c5c22d027513f924f07
Symphoriano Martins Branco <104242815+SymMartins@users.noreply.github.com>
2022-05-25 15:03:29 -0300

    Adiconado a resolução do Prof. Guanabara! Pouca diferença do qual eu fiz. O prof. utilizou o dados['situação'] para colocar a situação de arpovado, reprovado ou de recuperação. E eu ja fiz uma variavel chamada resultado ao invez de colocar tudo no dicionário! o resultado foi o mesmo porém a resolução do Prof. foi mais limpa e com menos linha de comando!!

--- PythonExercicios/mundo_tres/ex090.py
+++ PythonExercicios/mundo_tres/ex090.py
@@ -8,14 +8,33 @@ resultado = 0
 dados['nome'] = str(input('Nome do aluno: ')).title().strip()
 dados['media'] = float(input(f'Média do {dados["nome"]} : '))
 # 3.º Verifica se está aprovado, reprovado ou em recuperação:
-if dados["media"] >= 8:
+if dados['media'] >= 7:
     resultado = 'Aprovado'
-elif 8 > dados["media"] >= 5:
+elif 7 > dados['media'] >= 5:
     resultado = 'Recuperação'
 else:
     resultado = 'Reprovado'
 # 4.º Mostra a estrutura na tela:
 print('-=' * 30)
-print(f'- Aluno: {dados["nome"]}')
-print(f'- A média do {dados["nome"]} é de: {dados["media"]}')
-print(f'- A sintuação do {dados["nome"]} é de: {resultado}')
+print(f'  - Aluno: {dados["nome"]}')
+print(f'  - A média do {dados["nome"]} é de: {dados["media"]}')
+print(f'  - A sintuação do {dados["nome"]} é de: {resultado}')
+
+# ------------------------------------------Resolução do Guanabara ---------------------------------------------------
+
+# 1.º O dicionário:
+dados = {}
+# 2.º Lê nome e média do aluno:
+dados['nome'] = str(input('Nome do aluno: ')).title().strip()
+dados['media'] = float(input(f'Média do {dados["nome"]} : '))
+# 3.º Verifica se está aprovado, reprovado ou em recuperação:
+if dados['media'] >= 7:
+    dados['situação'] = 'Aprovado'
+elif 7 > dados['media'] >= 5:
+    dados['situação'] = 'Recuperação'
+else:
+    dados['situação'] = 'Reprovado'
+# 4.º Mostra a estrutura na tela:
+print('-=' * 30)
+for k, v in dados.items():
+    print(f'  - {k} é igual a {v}')
