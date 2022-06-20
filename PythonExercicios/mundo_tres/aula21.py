b174362a299474f32c18b2d73db70f787f7e426c
Symphoriano Martins Branco <104242815+SymMartins@users.noreply.github.com>
2022-06-20 09:01:41 -0300

    Aula 21:
    Nessa aula, vamos continuar nossos estudos de funções em Python, aprendendo mais sobre Interactive Help em Python, o uso de docstrings para documentar nossas funções, argumentos opcionais para dar mais dinamismo em funções Python, escopo de variáveis e retorno de resultados.

---
+++ PythonExercicios/mundo_tres/aula21.py
@@ -0,0 +1,3 @@
+"""Nessa aula, vamos continuar nossos estudos de funções em Python, aprendendo mais sobre Interactive Help em Python,
+o uso de docstrings para documentar nossas funções, argumentos opcionais para dar mais dinamismo em funções Python,
+escopo de variáveis e retorno de resultados."""
